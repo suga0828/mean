bde103a1d549c46896f3c08758464a33357e7074
alexander.sf0828@gmail.com <alexander.sf0828@gmail.com>
2019-05-03 14:16:09 -0500

    Crear prueba Actions Component (100% covertura, muestra error en expect)

--- src/app/components/actions/actions.component.spec.ts
+++ src/app/components/actions/actions.component.spec.ts
@@ -1,14 +1,30 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ActionsComponent } from './actions.component';
+import { MatBottomSheetRef } from '@angular/material';
+import { PinsService } from '../pins/pins.service';
+import { NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
-describe('ActionsComponent', () => {
+class MatBottomSheetStub {
+  dismiss() { }
+}
+
+class PinsServiceStub {
+  resolveActionObserver(e) { }
+}
+
+fdescribe('ActionsComponent', () => {
   let component: ActionsComponent;
   let fixture: ComponentFixture<ActionsComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ActionsComponent ]
+      declarations: [ ActionsComponent ],
+      providers: [
+        { provide: MatBottomSheetRef, useClass: MatBottomSheetStub},
+        { provide: PinsService, useClass: PinsServiceStub},        
+      ],
+      schemas: [NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA]
     })
     .compileComponents();
   }));
@@ -21,5 +37,15 @@ describe('ActionsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should open', () => {
+    const action = 'any';
+    const event = new MouseEvent('click');
+    const openLink = spyOn((<any>component).bottomSheetRef, 'dismiss');
+
+    component.openLink(event, action);
+
+    expect(openLink).toHaveBeenCalledWith(event, action);
   });
 });
