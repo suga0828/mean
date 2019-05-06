fc37b61f5fef995d5dd1aaca97260efbfaf974f7
alexander.sf0828@gmail.com <alexander.sf0828@gmail.com>
2019-05-06 14:03:20 -0500

    Crear prueba del metodo GET de ApiService

--- src/app/services/api.service.spec.ts
+++ src/app/services/api.service.spec.ts
@@ -2,8 +2,9 @@ import { TestBed, getTestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { ApiService } from './api.service';
+import { environment } from 'src/environments/environment';
 
-describe('ApiService', () => {
+fdescribe('ApiService', () => {
   let service: ApiService;
   let injector: TestBed;
   let httpMock: HttpTestingController;
@@ -12,13 +13,36 @@ describe('ApiService', () => {
     providers: [ApiService],
     imports: [HttpClientTestingModule]
   }));
+
   beforeEach( () => {
     service = TestBed.get(ApiService);
     injector = getTestBed();
     httpMock = injector.get(HttpTestingController);
   });
 
+  afterAll( () => {
+    service = null;
+    injector = null;
+    httpMock = null;
+  });
+
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('GET', () => {
+    it('Should execute GET', () => {
+      const result = 'testing';
+
+      service.get('/test')
+        .subscribe( response => {
+          expect(response).toBe(result);
+        });
+
+      const req = httpMock.expectOne(environment.apiEndpoint + '/test');
+      //- console.log(req);
+      expect(req.request.method).toBe('GET');
+      req.flush(result);
+    });
   });
 });
